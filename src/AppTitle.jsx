06fbb0b7e51efa47b036f19f90ada6f7e75de3c3
NikitaPW <n-zaxarov99@mail.ru>
2019-10-22 12:03:04 +0200

    Lab_2_First_Task. Right now I'm splitting the tasks into the stages, since the las t lab was checked and it appeared to be obligatory. So all the subsequent commits will present each stage of my solution for this lab.

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -127,19 +127,18 @@ class Input extends React.Component{
       //generateArrayq=(a,b)=>{ return Array(b-a).fill().map((item,index)=>1+index)}
       constructor(props) {
         super(props);
-        this.state={a:0, b:0, arr:[],hide:false};
-        this.counter=0;
-        this.handleClick = this.handleClick.bind(this)
+        this.state={a:0, b:0, arr:[]};
       }
 
      changea(ev){
-        this.setState({a: ev.target.value}, ()=>{console.log("Value of a was changed to " +this.state.a );
+        this.setState({a: ev.target.value}, ()=>{console.log("Value of a was changed to " + this.state.a );
         if(this.state.a < this.state.b&&this.state.a>0)
             this.setState({arr:generateArrayFromAtoB(this.state.a,this.state.b)});
         else {
             this.setState({arr:[]});}
         });
      }
+
      changeb(ev){
         this.setState({b: ev.target.value}, ()=>{console.log("Value of a was changed to " + this.state.b);
         if(this.state.a < this.state.b&&this.state.a>0)
@@ -148,28 +147,19 @@ class Input extends React.Component{
             this.setState({arr:[]});}
         });
       }
-     handleClick(){
-        if(this.state.hide==true)
-            this.setState({hide:false});
-        else
-            this.setState({hide:true});
-     }
+
      render(){
      const squared = squaredElements(this.state.arr);
      return(
      <div>
-     {console.time("Render"+this.counter)}
          <h1>LAB_2</h1>
          <h2>Input 1({this.state.a}):</h2>
          <input type="number" pattern="[0-9]*" placeholder="Only numbers" onChange={this.changea.bind(this)}/>
          <h2>Input 2({this.state.b}):</h2>
          <input type="number" pattern="[0-9]*" placeholder="Only numbers" onChange={this.changeb.bind(this)}/>
-         <h4>The state a is {this.state.a}</h4>
-         <h4>The state b is {this.state.b}</h4>
-         <p>{this.state.arr.map(item=>item+ " ")}</p>
-         <button onClick={this.handleClick}>Process Array</button>
-         <p >{this.state.hide ? squared.map(item=> item + " "):null}</p>
-     {console.timeEnd("Render"+this.counter++)}
+
+
+
      </div>
      );
      }
@@ -180,27 +170,10 @@ class Input extends React.Component{
 
 class AppTitle extends React.Component{
     render(){
-    const arr = generateArray(10);
-    const arrRand = generateRandomNumbers();
-    const arrGiven1 = [2,56,23,88,17,4];
-    const arrGiven2 = [2,5,8,10];
-    const squared = squaredElements(arrGiven2);
+
     return(
     <div>
-    <h1>LAB_1</h1>
-    <h2>Array from 1 to n</h2>
-    <div>{arr.map(item=> item + " ")}</div>
-    <h2>Arrays out of 25 random numbers</h2>
-    <div>{arrRand.map(item=>item + " ")}</div>
-    <h2>Elements that are larger then 15</h2>
-    <div>{arrGiven1.map(item =>item+" ").filter(item=>{return(item>15)})}</div>
-    <h2>Array with squared elements</h2>
-    <div>{squared.map(item =>item+" ")}</div>
-    <h2>Students</h2>
-    <button onClick={showStudents}>Show students</button>
-    <button onClick={sortStudents}>Sort students</button>
-    <button onClick={oldStudents}>Old students</button>
-    <ul id="showStudents"></ul>
+
     <Input />
     </div>
     );
