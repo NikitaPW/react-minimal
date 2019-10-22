65781915ace5e4d15872dd205df90b707842a265
NikitaPW <n-zaxarov99@mail.ru>
2019-10-22 14:09:57 +0200

    Lab_2_Fourth_Task. Optimized.

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -1,43 +1,45 @@
 import React from 'react'
 
-const generateArray = (n) => {return Array(n).fill().map((item,index)=>1+index)}
+const generateArray = (n) => Array(n).fill().map((item,index)=>1+index)
 
-const generateArrayFromAtoB=(a,b)=>{ return Array.from(generateArray(b-a+1),item=>+ item + - 1 + +a)}
+const generateArrayFromAtoB=(a,b)=> Array.from(generateArray(b-a+1),item=>+ item + - 1 + +a)
 
-const squaredElements=(arr)=>Array(arr.length).fill().map((item,index)=>arr[index]*arr[index])
+const squaredElements=(arr)=> Array(arr.length).fill().map((item,index)=>arr[index]*arr[index])
 
 class Input extends React.Component{
       constructor(props) {
         super(props);
-        this.state={a:0, b:0, arr:[],hide:false};
+        this.state={a:0, b:0, arr:[], squared:[]};
         this.counter=0;
         this.handleClick = this.handleClick.bind(this)
       }
 
      changea(ev){
-        this.setState({a: ev.target.value}, ()=>{console.log("Value of a was changed to " +this.state.a );
-        if(this.state.a < this.state.b&&this.state.a>0)
+        this.setState({a: ev.target.value}, ()=>{console.log("Value of a was changed to " + this.state.a );
+        if(this.state.a < this.state.b&&this.state.a>0){
             this.setState({arr:generateArrayFromAtoB(this.state.a,this.state.b)});
+
+             }
         else {
-            this.setState({arr:[]});}
+            this.setState({arr:[]});
+            }
         });
      }
      changeb(ev){
         this.setState({b: ev.target.value}, ()=>{console.log("Value of a was changed to " + this.state.b);
-        if(this.state.a < this.state.b&&this.state.a>0)
+        if(this.state.a < this.state.b&&this.state.a>0){
             this.setState({arr:generateArrayFromAtoB(this.state.a,this.state.b)});
+            }
         else {
             this.setState({arr:[]});}
         });
       }
      handleClick(){
-        if(this.state.hide==true)
-            this.setState({hide:false});
-        else
-            this.setState({hide:true});
+            this.setState({squared:squaredElements(this.state.arr)});
+
      }
+
      render(){
-     const squared = squaredElements(this.state.arr);
      return(
      <div>
      {console.time("Render"+this.counter)}
@@ -48,7 +50,7 @@ class Input extends React.Component{
          <input type="number" pattern="[0-9]*" placeholder="Only numbers" onChange={this.changeb.bind(this)}/>
          <p>{this.state.arr.map(item=>item+ " ")}</p>
          <button onClick={this.handleClick}>Process Array</button>
-         <p>{this.state.hide ? squared.map(item=> item + " "):null}</p>
+         <p>{ this.state.squared.map(item=>item + " ")}</p>
      {console.timeEnd("Render"+this.counter++)}
      </div>
      );
